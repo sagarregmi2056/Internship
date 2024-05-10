70f00f0bd2a435b621b2ea68e926f3d539144880
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-05-10 11:52:03 +0545

    added dom

---
+++ javascriptday1/index.js
@@ -0,0 +1,31 @@
+// step 1 : let const var
+// step 2: country ,name, age ,gender
+
+// step 3: value "nepal" , "anurag" , 23 , "male"
+
+// console.log(name);
+// {
+//   var name = "anurag";
+//   name = "nepal";
+// }
+
+// console.log(name);
+
+// var numberone = 10;
+// var numbertwo = 20;
+
+// var sum = numberone + numbertwo;
+// var subtract = numberone - numbertwo;
+// var multiply = numberone * numbertwo;
+
+// console.log(sum);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue",
+// };
+// console.table(person);
+
+// my name is sanjok karki i live in chitwan i am 20 yrs old
