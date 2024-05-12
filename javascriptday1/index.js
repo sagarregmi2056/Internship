342d9767657828a8398549322e0d50bb0cd2049c
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-05-12 11:25:07 +0545

    added daytwojs

--- javascriptday1/index.js
+++ javascriptday1/index.js
@@ -29,3 +29,26 @@
 // console.table(person);
 
 // my name is sanjok karki i live in chitwan i am 20 yrs old
+
+// step1: let const var
+// step2:bottle pen book name country age category
+// step3:value "nepal" , "anurag" , 23 , "male"
+
+// var numberone = 30;
+// var numbertwo = 20;
+
+// var sum = numberone + numbertwo;
+// var multiply = numberone * numbertwo;
+// var divide = numberone / numbertwo;
+// var subtract = numberone - numbertwo;
+
+// console.log(`the sum of a and b is ${sum} `);
+// console.log("the multiple of a and b is" + " " + multiply);
+
+// console.log(numberone / numbertwo);
+
+function callmyname(name, age) {
+  console.log(`Hi  ${name} happy ${age}th birthday takecare`);
+}
+
+callmyname("sujata", 23);
---
+++ javascriptday2/index.js
@@ -0,0 +1,52 @@
+// const item = {
+//   itemName: "sanjok",
+//   price: 400,
+//   description: "this is good item",
+//   Color: "blue",
+//   fullDiscription: function () {
+//     return this.itemName + " " + this.Color + " " + this.description;
+//   },
+// };
+
+// console.log(item.fullDiscription());
+
+// // console.log(item.itemName);
+
+// // console.log(item["price"]);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+
+// };
+// console.log(person.fullName());
+
+// name age bloodgroup country disease
+
+// fulldescription()
+
+var name = "sagar";
+var lastName = "regmi";
+
+// console.log(name == lastName);
+
+// console.log(name == lastName ? "yes" : "no");
+
+var bookPrice = 8085;
+var microprocessor = "8085";
+
+var fullname = bookPrice - microprocessor;
+
+console.log(typeof bookPrice);
+console.log(typeof microprocessor);
+
+console.log(typeof fullname);
+
+// conditional statement
+
+console.log(bookPrice === microprocessor ? "yes" : "no");
