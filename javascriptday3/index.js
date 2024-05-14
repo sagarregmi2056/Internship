91090b398cfd184c2f4abf118de2a3dd659b6cf7
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-05-14 22:18:27 +0545

    added days3js

---
+++ javascriptday3/index.js
@@ -0,0 +1,204 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "sagar"];
+
+// console.log(fruits);
+
+// below both are same
+// console.log(fruits[4]);
+// console.log(fruits[fruits.length - 1]);
+// console.log(fruits.at(4));
+
+// find the index of banana where it lies
+// console.log(fruits.indexOf("sagar"));
+
+// console.log(fruits.join(" @  "));
+
+// let fruit = fruits.at(2);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "sagar"];
+
+// const newFruits = fruits.pop();
+// console.log(newFruits);
+// console.log(fruits);
+// const popmango = fruits.pop();
+// console.log(popmango);
+// console.log(fruits);
+// const removebanana = fruits.shift();
+// console.log(removebanana);
+// console.log(fruits);
+// const popmangoo = fruits.pop();
+// console.log(popmangoo);
+// console.log(fruits);
+// const popmanggo = fruits.pop();
+// console.log(popmanggo);
+// console.log(fruits);
+
+// const pushingone = fruits.push("Banana");
+// console.log(fruits);
+// const fromfirstapple = fruits.unshift("apple");
+// console.log(fruits);
+
+// const removefromfirst = fruits.shift();
+// console.log(fruits);
+
+// const replce = fruits.copyWithin(0, 2);
+
+// console.log(replce);
+
+// console.log(fruits);
+
+// const nayaarray = fruits.flat();
+// console.log(nayaarray);
+
+// console.log(fruits.includes("Apple"));
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "sagar"];
+
+// const newfruits = fruits.splice(3, 2, "grapes");
+
+// splice have three parameter (which index ma rakhney ,tyaspaxi kati ota dlt garni ,something like string number )
+// console.log(fruits);
+
+// const student = ["sujata", "amrit", "sagar", "anurag", "umesh", "pragyan"];
+
+// console.log(student[0]);
+// console.log(student.at(0));
+// console.log(student.includes("umeshji"));
+// console.log(student.indexOf("umesh"));4
+// const pragayanlaiclassbataNikaldim = student.pop();
+// console.log(student, student.pop());
+
+// const pragayanlaihaldeu = student.push("pragyan");
+// console.log(student);
+
+// const fruits = [
+//   "apple",
+//   "banana",
+//   "orange",
+//   "grape",
+
+//   "kiwi",
+//   "pineapple",
+
+//   "strawberry",
+//   "blueberry",
+//   "watermelon",
+//   "peach",
+//   "mango",
+//   "pear",
+
+//   "cherry",
+//   "plum",
+//   "lemon",
+//   "lime",
+//   "pomegranate",
+//   "apricot",
+//   "nectarine",
+//   "coconut",
+//   "raspberry",
+//   "blackberry",
+//   "cranberry",
+//   "fig",
+//   "guava",
+//   "passionfruit",
+//   "melon",
+//   "tangerine",
+//   "dragonfruit",
+//   "papaya",
+//   "avocado",
+//   "starfruit",
+//   "lychee",
+//   "persimmon",
+//   "grapefruit",
+//   "honeydew",
+//   "cantaloupe",
+//   "date",
+//   "kiwifruit",
+//   "mulberry",
+//   "jackfruit",
+//   "apricot",
+//   "boysenberry",
+//   "elderberry",
+//   "gooseberry",
+//   "kiwi",
+//   "durian",
+//   "mangosteen",
+//   "olive",
+//   "quince",
+//   "rhubarb",
+//   "soursop",
+//   "ugli fruit",
+//   "breadfruit",
+//   "cherimoya",
+//   "salak",
+//   "longan",
+//   "horned melon",
+//   "ugli fruit",
+//   "quince",
+//   "loquat",
+//   "maracuja",
+//   "kumquat",
+//   "tamarillo",
+//   "satsuma",
+//   "soursop",
+//   "tamarind",
+//   "ugli fruit",
+//   "physalis",
+//   "feijoa",
+//   "rambutan",
+//   "ackee",
+//   "pawpaw",
+//   "tamarillo",
+//   "jabuticaba",
+//   "durian",
+//   "soursop",
+//   "longan",
+//   "ugli fruit",
+//   "feijoa",
+//   "carambola",
+//   "mangosteen",
+//   "pitaya",
+//   "ugli fruit",
+//   "quince",
+//   "kumquat",
+//   "pomelo",
+//   "cactus fruit",
+//   "ugli fruit",
+//   "feijoa",
+//   "tamarind",
+//   "guava",
+//   "ugli fruit",
+//   "feijoa",
+// ];
+
+// const newarray = fruits.splice(4, 3, "amrit");
+// console.table(fruits);
+
+// console.log(fruits.indexOf("cherry"));
+
+// const addamrit = fruits.splice(12, 0, "amrit");
+// console.table(fruits);
+
+// console.table(fruits.sort());
+
+// ternary operator (expression ? "expression"cherrry" : "expression"cherrry")
+// console.log();
+// console.log(fruits[fruits.length - 1]);
+
+// const sagarlaiagadi = fruits.unshift("sagar");
+// console.table(fruits);
+
+// const removesagar = fruits.shift();
+// const removesagar1 = fruits.shift();
+// const removesagar2 = fruits.shift();
+// const removesagar3 = fruits.shift();
+// const removesagar4 = fruits.shift();
+// console.table(fruits);
+// const copyelement = fruits.copyWithin(3, 0);
+// console.table(fruits);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = "sagar";
+fruits[fruits.length] = "sagar1";
+
+// const copyvalue = fruits.copyWithin(3, 0);
+console.log(fruits);
