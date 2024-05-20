54bd48513a15521e8eebbb4544a4cb3b238a13fb
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-05-20 12:24:13 +0545

    added data

---
+++ apiconsumption3/index.js
@@ -0,0 +1,16 @@
+// assigning object attributes to variables
+const person = {
+  name: "Sara",
+  age: 25,
+  gender: "female",
+};
+
+// const { name, age, gender } = person;
+
+let sname = person.name;
+let age = person.age;
+let gender = person.gender;
+
+console.log(sname); // Sara
+console.log(age); // 25
+console.log(gender); // female
